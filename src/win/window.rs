af2f8feecff29fa47e3332e58e8a4bb7f8069b93
AZ Software <66299945+AZProductions@users.noreply.github.com>
2024-03-29 17:08:50 +0100

    chore; implement WM_ERASEBKGND

--- src/win/window.rs
+++ src/win/window.rs
@@ -10,7 +10,7 @@ use winapi::um::winuser::{
     ReleaseCapture, SetCapture, SetProcessDpiAwarenessContext, SetTimer, SetWindowLongPtrW,
     SetWindowPos, SetFocus, GetFocus, TrackMouseEvent, TranslateMessage, UnregisterClassW, CS_OWNDC,
     GET_XBUTTON_WPARAM, GWLP_USERDATA, IDC_ARROW, MSG, SWP_NOMOVE, SWP_NOZORDER, TRACKMOUSEEVENT,
-    WHEEL_DELTA, WM_CHAR, WM_CLOSE, WM_CREATE, WM_DPICHANGED, WM_INPUTLANGCHANGE, WM_KEYDOWN,
+    WHEEL_DELTA, WM_CHAR, WM_CLOSE, WM_CREATE, WM_DPICHANGED, WM_ERASEBKGND, WM_INPUTLANGCHANGE, WM_KEYDOWN,
     WM_KEYUP, WM_LBUTTONDOWN, WM_LBUTTONUP, WM_MBUTTONDOWN, WM_MBUTTONUP, WM_MOUSEHWHEEL,
     WM_MOUSELEAVE, WM_MOUSEMOVE, WM_MOUSEWHEEL, WM_NCDESTROY, WM_RBUTTONDOWN, WM_RBUTTONUP,
     WM_SHOWWINDOW, WM_SIZE, WM_SYSCHAR, WM_SYSKEYDOWN, WM_SYSKEYUP, WM_TIMER, WM_USER,
@@ -428,7 +428,10 @@ unsafe fn wnd_proc_inner(
         BV_WINDOW_MUST_CLOSE => {
             DestroyWindow(hwnd);
             Some(0)
-        }
+        },
+        WM_ERASEBKGND => {
+            Some(1)
+        },
         _ => None,
     }
 }
